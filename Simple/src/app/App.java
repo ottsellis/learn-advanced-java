2564efd169f9a7acf6c7c26671e1b831b5b7a59a
ottsellis <ott.sellis@gmail.com>
2022-09-14 16:12:23 +0300

    added some !!!

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,10 +2,6 @@ package app;
 
 class App {
     public static void main(String[] args){
-<<<<<<< HEAD
-        System.out.println("Hello world!!!");
-=======
-        System.out.println("Hello world!");
->>>>>>> 6eafd84b34a4746c5bece647c6b5007c22095af0
+        System.out.println("Hello world!!!!!");
     }
 }
