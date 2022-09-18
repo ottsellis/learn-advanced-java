b446d494dd145b52fba7604fe1719b6566f667e3
ottsellis <ott.sellis@gmail.com>
2022-09-18 14:38:30 +0300

    Created

---
+++ HelloWorldMaven/src/main/java/local/learnJavaProject/App.java
@@ -0,0 +1,13 @@
+package local.learnJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
