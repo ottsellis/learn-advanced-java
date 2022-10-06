e99f385d3cf0938b6a7a82669ebb0a168c2c085b
ottsellis <ott.sellis@gmail.com>
2022-10-06 16:46:24 +0300

    80. Creating test users - done

--- mysql/src/test/java/com/caveofprogramming/db/mysql/UserDaoImplTest.java
+++ mysql/src/test/java/com/caveofprogramming/db/mysql/UserDaoImplTest.java
@@ -8,66 +8,75 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.Before;
 
 public class UserDaoImplTest {
-	
+
 	private Connection conn;
 	private List<User> users;
-	
+
 	private static final int NUM_TEST_USERS = 1000;
-	
-	private List<User> loadUsers() throws IOException{
-		
-		var temp = Files.lines(Paths.get("../greatexpectations.txt"));
-		
-		temp.forEach(System.out::println);
-		
-		return null;
+
+	private List<User> loadUsers() throws IOException {
+
+		// @formatter:off
+		return Files
+			.lines(Paths.get("../greatexpectations.txt"))
+			.map(line -> line.split("[^A-Za-z]"))
+			.map(Arrays::asList)
+			.flatMap(list -> list.stream())
+			.filter(word -> word.length() > 3 && word.length() < 20)
+			.map(word -> new User(word))
+			.limit(NUM_TEST_USERS)
+			.collect(Collectors.toList());
+			
+		// @formatter:on
 	}
 
 	@Before
 	public void setUp() throws SQLException, IOException {
-		
+
 		users = loadUsers();
-		
+
 		var props = Profile.getProperties("db");
-		
+
 		var db = Database.instance();
-		
+
 		db.connect(props);
-		
+
 		conn = db.getConnection();
 		conn.setAutoCommit(false);
 	}
 
 	@After
 	public void tearDown() throws SQLException {
-		Database.instance().close();		
+		Database.instance().close();
 	}
-	
+
 	@Test
 	public void testSave() throws SQLException {
 		User user = new User("TestUserDaoImpl");
-		
+
 		UserDao userDao = new UserDaoImpl();
-		
+
 		userDao.save(user);
 		var stmt = conn.createStatement();
-		
-		var rs = stmt.executeQuery("SELECT id, name FROM user ORDER BY id DESC");	
-		
+
+		var rs = stmt.executeQuery("SELECT id, name FROM user ORDER BY id DESC");
+
 		var result = rs.next();
-		
+
 		assertTrue("cannot retrieve inserted user", result);
-		
+
 		String name = rs.getString("name");
-		
-		assertEquals("user name does not match retrieved",user.getName(),name);
-		
+
+		assertEquals("user name does not match retrieved", user.getName(), name);
+
 		stmt.close();
 	}
 }
